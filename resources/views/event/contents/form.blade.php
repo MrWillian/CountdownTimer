2436604f0eaba987b1416bb0c46b9926bc8ac578
MrWilllian <willian.dev10@gmail.com>
2020-05-27 16:40:03 -0300

    resource: add event form content

---
+++ resources/views/event/contents/form.blade.php
@@ -0,0 +1,12 @@
+<form action="" method="POST">
+  <h3>Evento</h3>
+  <label>Nome:</label>
+  <input type="text" name="name" id="name" />
+
+  <label>Data:</label>
+  <input type="text" name="data" id="data" />
+
+  <input type="text" name="time" id="time" />
+
+  <button>Iniciar</button>
+</form>
