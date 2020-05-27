64a2c6e84b1d09b3748caf471ea4d49eb71e7ecf
MrWilllian <willian.dev10@gmail.com>
2020-05-27 16:40:41 -0300

    resource: add event show content

---
+++ resources/views/event/contents/show.blade.php
@@ -0,0 +1,28 @@
+<div>
+  <h3>Nome do Evento</h3>
+  
+  <div id="countdown">
+    <div id="day_container">
+      <h3>1</h3>
+      <div>----</div>
+      <h4>Dia(s)</h4>
+    </div>
+    <div id="hour_container">
+      <h3>1</h3>
+      <div>----</div>
+      <h4>Hora(s)</h4>
+    </div>
+    <div id="minute_container">
+      <h3>1</h3>
+      <div>----</div>
+      <h4>Minuto(s)</h4>
+    </div>
+    <div id="second_container">
+      <h3>1</h3>
+      <div>----</div>
+      <h4>Segundo(s)</h4>
+    </div>
+  </div>
+
+  <button>Novo Evento</button>
+</div>
