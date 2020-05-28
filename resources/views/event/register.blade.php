64d36221ac14d6067660c101af9f8330b524faa4
MrWilllian <willian.dev10@gmail.com>
2020-05-28 16:04:52 -0300

    refactor: add container to register

--- resources/views/event/register.blade.php
+++ resources/views/event/register.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.event')
 
 @section('body_content')
-  <div class="content">
+  <div class="justify-content-center align-items-center container">
 
-    @include('event.contents.form')
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        @include('event.contents.form')
+      </div>
+    </div>
 
   </div>
-
 @endsection
