7b898c9b3585cd045cefe1ca717eb30f69a4c2ee
MrWilllian <willian.dev10@gmail.com>
2020-05-28 16:05:28 -0300

    refactor: add bootstrap structure to form content

--- resources/views/event/contents/form.blade.php
+++ resources/views/event/contents/form.blade.php
@@ -1,12 +1,21 @@
 <form action="" method="POST">
-  <h3>Evento</h3>
-  <label>Nome:</label>
-  <input type="text" name="name" id="name" />
+  <h3 class="title text-center">Evento</h3>
+  <div class="input-group">
+    <label class="col-sm-2 control-label label" for="name">Nome</label>
+    <div class="col-sm-10">
+      <input type="text" name="name" id="name" class="form-control input-lg" aria-describedby="helpBlockName" />
+      <span id="helpBlockName" class="help-block">Informe o nome do evento.</span>
+    </div>
+  </div>
 
-  <label>Data:</label>
-  <input type="text" name="data" id="data" />
+  <div class="input-group">
+    <label class="col-sm-2 control-label label" for="data">Data</label>
+    <div class="col-sm-10">
+      <input type="text" name="data" id="data" placeholder="__/__/____" class="form-control" />
+      <input type="text" name="time" id="time" placeholder="00:00:00" class="form-control" />
+      <span id="helpBlockData" class="help-block">Informe o dia seguido do horário do evento.</span>
+    </div>
+  </div>
 
-  <input type="text" name="time" id="time" />
-
-  <button>Iniciar</button>
+  <button id="start" type="submit" class="btn btn-lg">Iniciar</button>
 </form>
