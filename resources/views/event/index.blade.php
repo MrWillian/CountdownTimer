779c53204729da54b4e30ddf0614dd2af68b5e34
MrWilllian <willian.dev10@gmail.com>
2020-05-29 20:18:45 -0300

    refactor: include event list content and centered all body content

--- resources/views/event/index.blade.php
+++ resources/views/event/index.blade.php
@@ -1,9 +1,15 @@
 @extends('layouts.event')
 
 @section('body_content')
-  <div class="content">
+  <div class="justify-content-center align-items-center container">
 
-    @include('event.contents.show')
+    <div class="row">
+      <div class="col-md-8 col-md-offset-2">
+        @include('event.contents.show')
+
+        @include('event.contents.event_list')
+      </div>
+    </div>
 
   </div>
 
