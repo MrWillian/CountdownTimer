c461b399309adadeddbae47a21bf5bda3b56466e
MrWilllian <willian.dev10@gmail.com>
2020-05-29 20:16:45 -0300

    refactor: add event list content

---
+++ resources/views/event/contents/event_list.blade.php
@@ -0,0 +1,10 @@
+<div class="content">
+  <h3 class="title text-center">Eventos</h3>
+  <ul class="list-group">
+    <li class="list-group-item"><a class="event_name" href="#">Teste 1</a></li>
+    <li class="list-group-item"><a class="event_name" href="#">Teste 2</a></li>
+    <li class="list-group-item"><a class="event_name" href="#">Teste 3</a></li>
+    <li class="list-group-item"><a class="event_name" href="#">Teste 4</a></li>
+    <li class="list-group-item"><a class="event_name" href="#">Teste 5</a></li>
+  </ul>
+</div>
