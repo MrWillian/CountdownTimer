ceaad42160f42268d2ca85a9deeefd4e33dbb66c
MrWilllian <willian.dev10@gmail.com>
2020-05-29 15:55:14 -0300

    refactor: add last show changes

--- resources/views/event/contents/show.blade.php
+++ resources/views/event/contents/show.blade.php
@@ -1,28 +1,24 @@
-<div>
-  <h3>Nome do Evento</h3>
+<div class="content">
+  <h3 class="title text-center">Nome do Evento</h3>
   
-  <div id="countdown">
-    <div id="day_container">
+  <div id="countdown" class="col-md-12">
+    <div id="day_container" class="column col-md-13">
       <h3>1</h3>
-      <div>----</div>
-      <h4>Dia(s)</h4>
+      <h4 class="label-countdown">Dia(s)</h4>
     </div>
-    <div id="hour_container">
+    <div id="hour_container" class="column col-md-13">
       <h3>1</h3>
-      <div>----</div>
-      <h4>Hora(s)</h4>
+      <h4 class="label-countdown">Hora(s)</h4>
     </div>
-    <div id="minute_container">
+    <div id="minute_container" class="column col-md-13">
       <h3>1</h3>
-      <div>----</div>
-      <h4>Minuto(s)</h4>
+      <h4 class="label-countdown">Minuto(s)</h4>
     </div>
-    <div id="second_container">
+    <div id="second_container" class="column col-md-13">
       <h3>1</h3>
-      <div>----</div>
-      <h4>Segundo(s)</h4>
+      <h4 class="label-countdown">Segundo(s)</h4>
     </div>
   </div>
 
-  <button>Novo Evento</button>
+  <button id="new" type="submit" class="btn btn-success btn-lg btn-layout">Novo Evento</button>
 </div>
