3a651fd276db2924aa86a166e8d1a8a9557e6a37
MrWilllian <willian.dev10@gmail.com>
2020-06-01 10:39:29 -0300

    refactor: add script

--- resources/views/layouts/event.blade.php
+++ resources/views/layouts/event.blade.php
@@ -25,4 +25,11 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
   
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('#name').focus();
+      $('#date').mask('00/00/0000');
+      $('#time').mask('00:00:00');
+    });
+  </script>
 </html>
