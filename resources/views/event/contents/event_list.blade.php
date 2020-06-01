bd116999c31ae28390b9d3d6b6341c439ea6aad6
MrWilllian <willian.dev10@gmail.com>
2020-06-01 10:55:08 -0300

    refactor: display events on view

--- resources/views/event/contents/event_list.blade.php
+++ resources/views/event/contents/event_list.blade.php
@@ -1,10 +1,12 @@
 <div class="content">
   <h3 class="title text-center">Eventos</h3>
   <ul class="list-group">
-    <li class="list-group-item"><a class="event_name" href="#">Teste 1</a></li>
-    <li class="list-group-item"><a class="event_name" href="#">Teste 2</a></li>
-    <li class="list-group-item"><a class="event_name" href="#">Teste 3</a></li>
-    <li class="list-group-item"><a class="event_name" href="#">Teste 4</a></li>
-    <li class="list-group-item"><a class="event_name" href="#">Teste 5</a></li>
+    @foreach($events as $event)
+      <li class="list-group-item">
+        <a class="event_name" href="#">
+          {{$event->name}} - {{$event->date}} {{$event->time}}
+        </a>
+      </li>
+    @endforeach
   </ul>
 </div>
