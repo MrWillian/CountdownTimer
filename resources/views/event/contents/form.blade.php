3b01e01b816db51a013efa0e11781401f544c2e5
MrWilllian <willian.dev10@gmail.com>
2020-06-01 10:39:05 -0300

    refactor: add errors messages

--- resources/views/event/contents/form.blade.php
+++ resources/views/event/contents/form.blade.php
@@ -1,5 +1,6 @@
 <div class="content">
-  <form action="" method="POST">
+  <form action="{{ route('event.store') }}" method="POST">
+    {{ csrf_field() }}
     <h3 class="title text-center">Evento</h3>
     <div class="input-group">
       <label class="col-sm-2 control-label label" for="name">Nome</label>
@@ -12,11 +13,21 @@
     <div class="input-group">
       <label class="col-sm-2 control-label label" for="data">Data</label>
       <div class="col-sm-10">
-        <input type="text" name="data" id="data" placeholder="__/__/____" class="form-control" />
-        <input type="text" name="time" id="time" placeholder="00:00:00" class="form-control" />
+        <input type="text" name="date" id="date" placeholder="__/__/____" class="form-control" />
+        <input type="text" name="time" id="time" value="24:00:00" class="form-control" />
         <span id="helpBlockData" class="help-block">Informe o dia seguido do horário do evento.</span>
       </div>
     </div>
+
+    @if ($errors->any())
+      <div class="alert alert-danger">
+          <ul>
+              @foreach ($errors->all() as $error)
+                  <li>{{ $error }}</li>
+              @endforeach
+          </ul>
+      </div>
+    @endif
 
     <button id="start" type="submit" class="btn btn-lg btn-layout">Iniciar</button>
   </form>
