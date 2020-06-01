bbc01c83d0765f61145231cb2adf1fdd1ca3c12f
MrWilllian <willian.dev10@gmail.com>
2020-06-01 10:35:39 -0300

    refactor: add EventRepository with insert method

---
+++ app/Http/Repositories/EventRepository.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Repositories;
+
+use App\Model\Event;
+use App\Http\Repositories\Types\BaseRepository;
+
+class EventRepository extends BaseRepository 
+{
+  protected $modelClass = Event::class;
+
+  public function insertEvent($data){
+    $event = new Event;
+    $event->name = $data['name'];
+    $event->date = $data['date'];
+    $event->time = $data['time'];
+    try {
+      $event->save();
+    } catch(Exception $ex) {
+      return new Exception($ex);
+    }
+  }
+}
