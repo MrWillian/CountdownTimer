24f54fae7d21b07e043dd167fa4abad172967fca
MrWilllian <willian.dev10@gmail.com>
2020-06-03 09:10:46 -0300

    refactor: add infos in event anchor

--- resources/views/event/contents/event_list.blade.php
+++ resources/views/event/contents/event_list.blade.php
@@ -3,8 +3,10 @@
   <ul class="list-group">
     @foreach($events as $event)
       <li class="list-group-item">
-        <a class="event_name" href="#">
-          {{$event->name}} - {{$event->date}} {{$event->time}}
+        <a class="event_name" href="#"
+          event="{{$event->name}}" 
+          date="{{$event->date}} {{$event->time}}">
+          {{$event->name}} - {{date('d-m-Y', strtotime($event->date))}} {{$event->time}}
         </a>
       </li>
     @endforeach
