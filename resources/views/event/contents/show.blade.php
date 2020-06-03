c6123e1813817dc65785f3a07680507e653e64e5
MrWilllian <willian.dev10@gmail.com>
2020-06-03 09:11:43 -0300

    refactor: implement selected event values

--- resources/views/event/contents/show.blade.php
+++ resources/views/event/contents/show.blade.php
@@ -1,21 +1,30 @@
 <div class="content">
-  <h3 class="title text-center">Nome do Evento</h3>
+  <h3 id="event_name" class="title text-center">
+    {{$selectedEvent != null ? $selectedEvent->name : 'Nome do Evento'}}
+  </h3>
+  
+  <input 
+    type="hidden" 
+    id="event-time"
+    name="event-time" 
+    class="event-time"
+    value="{{ $selectedEvent != null ? ($selectedEvent->date . ' ' . $selectedEvent->time) : null }}" />
   
   <div id="countdown" class="col-md-12">
     <div id="day_container" class="column col-md-13">
-      <h3>1</h3>
+      <h3 class="label-days"></h3>
       <h4 class="label-countdown">Dia(s)</h4>
     </div>
     <div id="hour_container" class="column col-md-13">
-      <h3>1</h3>
+      <h3 class="label-hours"></h3>
       <h4 class="label-countdown">Hora(s)</h4>
     </div>
     <div id="minute_container" class="column col-md-13">
-      <h3>1</h3>
+      <h3 class="label-minutes"></h3>
       <h4 class="label-countdown">Minuto(s)</h4>
     </div>
     <div id="second_container" class="column col-md-13">
-      <h3>1</h3>
+      <h3 class="label-seconds"></h3>
       <h4 class="label-countdown">Segundo(s)</h4>
     </div>
   </div>
