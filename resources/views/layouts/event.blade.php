5b4c15b50e9e8687de7f8ba5a00866c7d9ec502d
MrWilllian <willian.dev10@gmail.com>
2020-06-03 09:12:12 -0300

    refactor: implement script to make Countdown event

--- resources/views/layouts/event.blade.php
+++ resources/views/layouts/event.blade.php
@@ -19,9 +19,8 @@
   @yield('body_content')
 
 </body>
-  <!-- Latest compiled and minified JavaScript -->
+
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
-  
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.11/jquery.mask.min.js"></script>
   
@@ -30,6 +29,55 @@
       $('#name').focus();
       $('#date').mask('00/00/0000');
       $('#time').mask('00:00:00');
+
+      calculateTime($('#event-time').val());
+
+      $('.event_name').click(function({ target }) {
+        $('#event-time').val($(target).attr('date'));
+        $('#event_name').html($(target).attr('event'));
+        calculateTime($('#event-time').val());
+      });
     });
+
+    var eventTime = $('#event-time').val();
+    var date;
+    var now = new Date(); 
+    var newYear = new Date('1.1.2020').getTime();
+    var startTimer = '';
+
+    function calculateTime(dates) {
+      clearInterval(startTimer);
+      
+      if(typeof(dates) == 'undefined'){
+        date = new Date(newYear).getTime();
+      }else {
+        date = new Date(dates).getTime();
+      }
+      
+      updateTimer(date);
+
+      startTimer = setInterval(function() { updateTimer(date); }, 1000);
+    }
+
+    function updateTimer(date) {
+      var now = new Date().getTime();
+      var distance = date - now;
+
+      var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      $('.label-days').html(days);
+      $('.label-hours').html(hours);
+      $('.label-minutes').html(minutes);
+      $('.label-seconds').html(seconds);
+
+      if(distance < 0){
+        clearInterval(startTimer);
+        $('#event_name').html($('#event_name').html() + ' - CONCLUÍDO');
+      }
+    }
+
   </script>
 </html>
